c161e23451a637a7d8ba57b05522acb8391320e6
SnO2WMaN <sno2wman.official@gmail.com>
2020-05-16 19:18:35 +0900

    feat: サイドメニューにリンクを追加

---
+++ src/components/NavMenu/Social.tsx
@@ -0,0 +1,59 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classnames from "classnames";
+import React from "react";
+import styled from "styled-components";
+import { Merge } from "type-fest";
+
+export type Props = Merge<ContainerProps, {}>;
+export const Component: React.FC<Props> = ({
+  className,
+  children,
+  icon,
+  href,
+}) => (
+  <li
+    className={classnames(
+      className,
+      "relative",
+      "hover:bg-gray-300",
+      "rounded-full"
+    )}
+  >
+    <div
+      className={classnames(
+        "absolute",
+        "inset-0",
+        "flex",
+        "justify-center",
+        "items-center"
+      )}
+    >
+      <FontAwesomeIcon
+        icon={icon}
+        className={classnames("text-xl", "text-gray-700")}
+      />
+    </div>
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      className={classnames("absolute", "inset-0")}
+    />
+  </li>
+);
+export const StyledComponent: typeof Component = styled(Component)`
+  padding-top: 100%;
+`;
+
+export type ContainerProps = {
+  className?: string;
+
+  href: string;
+  icon: IconDefinition;
+};
+export const SocialButton: React.FC<ContainerProps> = (props) => {
+  return <StyledComponent {...props} />;
+};
+
+export default SocialButton;
--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -1,4 +1,10 @@
 import {
+  faTwitter,
+  faGithub,
+  faSpeakerDeck,
+  faSteam,
+} from "@fortawesome/free-brands-svg-icons";
+import {
   faHome,
   faBriefcase,
   faIdCard,
@@ -9,11 +15,28 @@ import styled from "styled-components";
 import { Merge } from "type-fest";
 
 import Link from "./Link";
+import Social from "./Social";
 
 export type Props = Merge<ContainerProps, {}>;
 export const Component: React.FC<Props> = ({ className }) => (
-  <nav className={classnames(className, "bg-gray-100", "flex", "items-center")}>
-    <ul className={classnames("w-full")}>
+  <nav
+    className={classnames(
+      className,
+      "bg-gray-100",
+      "flex",
+      "flex-col",
+      "overflow-hidden"
+    )}
+  >
+    <ul
+      className={classnames(
+        "flex-grow",
+        "flex",
+        "flex-col",
+        "justify-center",
+        "w-full"
+      )}
+    >
       <Link link="/" icon={faHome}>
         Home
       </Link>
@@ -23,6 +46,22 @@ export const Component: React.FC<Props> = ({ className }) => (
       <Link link="/work" icon={faBriefcase}>
         Works
       </Link>
+    </ul>
+    <ul
+      className={classnames(
+        "w-full",
+        "grid",
+        "grid-cols-4",
+        "col-gap-4",
+        "row-gap-2",
+        "px-6",
+        "pb-6"
+      )}
+    >
+      <Social icon={faTwitter} href="https://twitter.com/SnO2WMaN" />
+      <Social icon={faGithub} href="https://github.com/SnO2WMaN" />
+      <Social icon={faSteam} href="https://steamcommunity.com/id/SnO2WMaN" />
+      <Social icon={faSpeakerDeck} href="https://speakerdeck.com/sno2wman" />
     </ul>
   </nav>
 );
