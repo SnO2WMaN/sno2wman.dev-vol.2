020eddcabc2fee43b101fc47d3cd941c93f4a0a5
SnO2WMaN <sno2wman.official@gmail.com>
2020-05-18 22:39:46 +0900

    fix: ページ遷移コンポーネントから不必要な部分を削除

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,21 +1,12 @@
 import classnames from "classnames";
-import { withRouter, useRouter } from "next/router";
-import React, { useContext, Fragment } from "react";
-import { useTransition, animated, config } from "react-spring";
-import styled from "styled-components";
-
-const Context = React.createContext();
-const Provider = ({ router, children }) => (
-  <Context.Provider value={router}>{children}</Context.Provider>
-);
-
-// const useRouter = () => useContext(Context);
-const RouterContextProvider = withRouter(Provider);
+import { useRouter } from "next/router";
+import React from "react";
+import { animated, useTransition, config } from "react-spring";
 
 const PageTransition = (props) => {
   const router = useRouter();
   const transitions = useTransition(router, (r) => r.pathname, {
-    config: { ...config.gentle },
+    config: config.gentle,
     from: { opacity: 0, transform: "translateY(-50%" },
     enter: { opacity: 1, transform: "translateY(0%)" },
     leave: { opacity: 0, transform: "translateY(50%)" },
@@ -24,14 +15,14 @@ const PageTransition = (props) => {
   return (
     <>
       {transitions.map(({ item, props: style, key }) => {
-        const a = item.components?.[item.pathname];
+        const before = (item as any).components?.[item.pathname];
         return (
           <animated.div
             key={key}
             style={style}
             className={classnames(props.className)}
           >
-            {a ? <a.Component {...a.prop} /> : props.children}
+            {before ? <before.Component {...before.prop} /> : props.children}
           </animated.div>
         );
       })}
