27e22e4d4e66e04cc0f74909ec58bb04d99456e2
SnO2WMaN <sno2wman.official@gmail.com>
2020-05-20 06:05:19 +0900

    perf: Tailwind CSSにPurgeCSSを対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "src/**/*.css",
+      "src/**/*.scss",
+      "src/**/*.sass",
+      "src/**/*.html",
+      "src/**/*.js",
+      "src/**/*.ts",
+      "src/**/*.jsx",
+      "src/**/*.tsx",
+    ],
+  },
   theme: {
     extend: {},
   },
